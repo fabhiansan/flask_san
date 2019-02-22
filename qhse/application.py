e0445a00b36f90242ba5ba79d785785b8c8f3708
fabhiansan <35937003+fabhiansan@users.noreply.github.com>
2019-02-22 17:02:52 +0800

    Add files via upload

---
+++ qhse/application.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+UPLOAD_FOLDER = 'Z:\htdocs\qhse\uploads'
+allowed_extensions = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'php', 'py'])
+
+app.config['upload_folder'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.',1)[1].lower() in allowed_extensions
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files :
+            flash('no file part')
+            return redirect(request.url)
+
+        if file.filename == '':
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config[upload_folder], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['upload_folder'], filename)
