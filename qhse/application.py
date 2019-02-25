225b93cd68639cefdacc0b120280d41edc3d1c93
fabhiansan <35937003+fabhiansan@users.noreply.github.com>
2019-02-25 21:17:46 +0700

    Add files via upload

--- qhse/application.py
+++ qhse/application.py
@@ -6,41 +6,20 @@ import ftplib
 
 app = Flask(__name__)
 
-@app.route('/listdir')
-def intro():
-    y = listdir(getcwd())
-    return render_template('listdir.html', **locals())
 
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template('index.html')
  
-@app.route("/hello/<string:name>/")
-def hello(name):
-    
-    quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ", "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ", "'To understand recursion you must first understand recursion..' -- Unknown", "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown", "'Mathematics is the key and door to the sciences.' -- Galileo Galilei", "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"  ]
-    randomNumber = randint(0,len(quotes)-1)
-    quote = quotes[randomNumber]
-    
-    class xy():
-        x = getcwd()
-        y = listdir(getcwd())
-    return render_template('test.html',**locals())
-
-@app.route('/jstree')
-def jstree():
-    return render_template('jstree.html')
-
 @app.route('/ftplib', methods=['POST', 'GET'])
 def ftplibs():
     if request.method == 'POST':
         resultname = request.form['usernames']
         resultpassword = request.form['passwords']
         
-        ftp = ftplib.FTP('192.168.34.10')
+        ftp = ftplib.FTP('localhost')
         ftp.login(user=resultname, passwd=resultpassword)
         
-        files = []
         x = ftp.nlst()
         
         return render_template('ftplib.html', x = x)
